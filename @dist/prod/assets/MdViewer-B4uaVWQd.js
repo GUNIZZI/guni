e4106fc116a0997941332eba445da7e7b74460cf
h2dlhs <h2dlhs@realsn.com>
2024-08-21 23:25:33 +0900

    배포 이슈 해결중

---
+++ @dist/prod/assets/MdViewer-B4uaVWQd.js
@@ -0,0 +1,24 @@
+import{c as e,j as a,a as l}from"./index-DeujlTZv.js";import{E as o}from"./scrollRestoration.store-BTb4QTa7.js";const t=()=>e`
+    .wmde-markdown {
+        background: none;
+
+        ul,
+        ol {
+            list-style-type: inherit;
+
+            ul,
+            ol {
+                list-style-type: circle;
+
+                ul,
+                ol {
+                    list-style-type: square;
+                }
+            }
+        }
+    }
+
+    pre[class^='language'] {
+        background: rgba(255, 255, 255, 0.05);
+    }
+`,c=({content:s,className:r})=>a("div",{"data-color-mode":"dark",css:t,className:r,children:[s,l(o.Markdown,{style:{padding:10},source:s})]});export{c as M};
