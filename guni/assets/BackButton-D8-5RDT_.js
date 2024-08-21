19a158d873e42775942928016dff21c878fc29b8
GUNIZZI <GUNIZZI@users.noreply.github.com>
2024-08-21 15:22:26 +0000

    deploy: 1729101fff322eb4db197203329302b2e0b7eccc

---
+++ guni/assets/BackButton-D8-5RDT_.js
@@ -0,0 +1,1 @@
+import{C as s,i as r,a as t,a0 as a}from"./index-D04QuwKA.js";const e=s(r.jsx("path",{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z"}),"ArrowBack"),c=({...o})=>t(a,{...o,sx:{position:"fixed",top:"1vh",left:"1vw"},children:t(e,{sx:{fontSize:"6rem"}})});export{c as B};
---
+++ guni/assets/index-DT3XoPub.js
@@ -0,0 +1,139 @@
+import{c as s,N as t,j as n,a as e,m as i}from"./index-D04QuwKA.js";import{E as r}from"./easeing-DYWzsbkc.js";const l="/guni/assets/bg1-CP3TVomi.jpg",c=a=>s`
+    // background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${l});
+    // background-size: cover;
+    // background-position: center;
+
+    background: radial-gradient(
+        circle at 20% 20%,
+        rgb(255 135 212 / 20%) 0%,
+        rgba(50, 50, 100, 0.2) 50%,
+        rgba(10, 10, 10, 1) 100%
+    );
+
+    .wrap {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: calc(100vh - 48px);
+
+        .subtitle {
+            padding: 0 0 2rem 0;
+            font-size: 1em;
+            font-weight: 200;
+
+            > span {
+                padding: 0 0.5em;
+                font-size: 0.75em;
+            }
+        }
+
+        .grpG {
+            display: flex;
+            align-items: center;
+            font-size: 1.4em;
+            text-align: center;
+
+            .g {
+                line-height: 1;
+                font-size: 10em;
+                font-weight: 400;
+                background: linear-gradient(
+                    135deg,
+                    #ff018e 20%,
+                    ${t[0].color} 70%,
+                    #fff 90%
+                );
+                color: transparent;
+                -webkit-background-clip: text;
+                background-clip: text;
+                -webkit-text-fill-color: transparent;
+                text-shadow: 0 0 30px rgba(255, 255, 255, 0.2);
+            }
+
+            .uni {
+                display: flex;
+                margin: 1.5em 0 0 -0.8em;
+
+                > * {
+                    font-size: 2em;
+                    font-weight: 500;
+                    line-height: 1;
+                }
+            }
+
+            // background: linear-gradient(
+            //     135deg,
+            //     ${t[0].color} 0%,
+            //     ${t[1].color} 20%,
+            //     ${t[2].color} 40%,
+            //     ${t[3].color} 60%,
+            //     ${t[4].color} 80%
+            // );
+            // color: transparent;
+            // -webkit-background-clip: text;
+            // background-clip: text;
+            // -webkit-text-fill-color: transparent;
+            // text-shadow: 0 0 60px rgba(255, 255, 255, 0.5);
+        }
+    }
+
+    .bg {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        font-family: sans-serif;
+        overflow: hidden;
+
+        &:before {
+            content: 'G';
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translateX(-50%) translateY(-50%) rotate(-30deg);
+            font-size: 150vw;
+            font-weight: 300;
+            line-height: 1;
+            opacity: 0.01;
+        }
+        // &:after {
+        //     content: 'G';
+        //     position: absolute;
+        //     top: 50%;
+        //     left: 50%;
+        //     transform: translateX(-50%) translateY(-50%) rotate(-45deg);
+        //     font-size: 250vh;
+        //     font-weight: 100;
+        //     line-height: 1;
+        //     opacity: 0.01;
+        // }
+    }
+
+    > * {
+        & div {
+            font-size: ${a.typography.h6.fontSize};
+        }
+
+        > strong {
+            font-size: ${a.typography.h2.fontSize};
+        }
+
+        > span {
+            font-size: ${a.typography.h3.fontSize};
+        }
+    }
+
+    .gradientText {
+        background: linear-gradient(
+            90deg,
+            ${t[0].color} 35%,
+            ${t[t.length-1].color} 75%
+        );
+        -webkit-background-clip: text;
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
+        text-fill-color: transparent;
+    }
+`,d={visible:{transition:{staggerChildren:.2,when:"beforeChildren"}}},g={hidden:{opacity:0,y:"1em"},visible:{opacity:1,y:0,transition:{duration:1,ease:r.EASE_OUT_QUART}}},p={hidden:{opacity:0,transform:"scale(1.2)"},visible:{opacity:1,transform:"scale(1)",transition:{duration:3,ease:r.EASE_OUT_QUART}}},o={hidden:{opacity:0,x:"0.4em"},visible:()=>({opacity:1,x:0,transition:{duration:1.2,ease:r.EASE_OUT_QUART}})},b=()=>n("div",{className:"page",css:c,children:[e("span",{className:"bg"}),n(i.div,{className:"wrap",variants:d,initial:"hidden",animate:"visible",children:[n(i.div,{className:"subtitle",variants:g,children:["Publisher ",e("span",{children:"&"})," Front End Developer."]}),n("div",{className:"grpG",children:[e(i.div,{className:"g",variants:p,children:"G"}),n("div",{className:"uni",children:[e(i.div,{variants:o,children:"U"}),e(i.div,{variants:o,children:"N"}),e(i.div,{variants:o,children:"I"})]})]})]})]});export{b as PageHome};
