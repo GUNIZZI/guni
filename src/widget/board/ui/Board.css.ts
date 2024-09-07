d5bc75720e3e4333d8446357f099e807872baa08
h2dlhs <h2dlhs@realsn.com>
2024-09-07 18:09:54 +0900

    게시판 Title 시각적으로 letterspaing 변경

--- src/widget/board/ui/Board.css.ts
+++ src/widget/board/ui/Board.css.ts
@@ -12,6 +12,7 @@ const boardStyle = (theme: Theme) => css`
         > h2 {
             font-size: 3rem;
             font-weight: 100;
+            letter-spacing: -0.03em;
 
             > strong {
                 color: var(--color);
@@ -44,67 +45,24 @@ const boardStyle = (theme: Theme) => css`
         }
     }
 
+    &.isList {
+        .header {
+            > h2 {
+                > strong {
+                    margin-right: -0.15em;
+                }
+            }
+        }
+    }
+
     &.isPf {
-        // .itemWrap {
-        //     .item {
-        //         flex-direction: column;
-        //         align-items: flex-start;
-        //         padding: ${theme.spacing(4)} ${theme.spacing(3)};
-        //         margin: 0;
-        //         word-break: break-word;
-        //         transition: all 0.18s;
-
-        //         &:hover {
-        //             background: rgba(255, 255, 255, 0.05);
-        //         }
-
-        //         > * {
-        //             margin: ${theme.spacing(0.2)} 0;
-        //         }
-
-        //         .title {
-        //             display: block;
-        //             padding: ${theme.spacing(2)} 0;
-        //             font-size: 2em;
-        //             font-weight: 200;
-        //             white-space: nowrap;
-        //             overflow: hidden;
-        //             text-overflow: ellipsis;
-
-        //             &:first-of-type {
-        //                 padding-top: 0;
-        //             }
-
-        //             &:link,
-        //             &:visited {
-        //                 color: inherit !important;
-        //                 text-decoration: none !important;
-        //             }
-
-        //             > .type {
-        //                 font-size: 0.45em;
-        //             }
-        //         }
-        //         .content {
-        //             display: block;
-        //             max-height: 100px;
-        //             color: rgba(255, 255, 255, 0.8);
-        //             font-weight: 300;
-        //             overflow: hidden;
-        //         }
-        //         .infos {
-        //             display: flex;
-        //             gap: 1.8em;
-        //             color: rgba(255, 255, 255, 0.4);
-        //             font-size: 0%.75;
-        //             font-weight: 200;
-        //         }
-        //     }
-        // }
-
-        // .MuiDivider-root {
-        //     margin: 0;
-        // }
+        .header {
+            > h2 {
+                > strong {
+                    margin-right: -0.17em;
+                }
+            }
+        }
     }
 
     &.isView {
